0c060015aaf9e958f3a9ade681e7aaa5e386ebb6
Ankichi <59509443+An39nA0219@users.noreply.github.com>
2021-11-02 10:32:00 +0900

    プロジェクト ファイルを追加します。

---
+++ CSTwitterCloneApi/Data/CSTwitterCloneApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CSTwitterCloneApi.Models;
+
+namespace CSTwitterCloneApi.Data
+{
+    public class CSTwitterCloneApiContext : DbContext
+    {
+        public CSTwitterCloneApiContext (DbContextOptions<CSTwitterCloneApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CSTwitterCloneApi.Models.User> User { get; set; }
+    }
+}
